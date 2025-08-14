4fbfd22c70f2a8c9af6ee116e80a163c2a9241fd
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-08-13 20:09:57 -0600

    Add SignStatement integration test
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

---
+++ signer_test.go
@@ -0,0 +1,92 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package signer
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/carabiner-dev/signer/bundle"
+	"github.com/carabiner-dev/signer/bundle/bundlefakes"
+	"github.com/carabiner-dev/signer/options"
+)
+
+func TestSignStatement(t *testing.T) {
+	t.Parallel()
+
+	attData := `{
+  "predicateType": "https://example.com/my-predicate/v1",
+  "predicate": { "something": "custom" },
+  "type": "https://in-toto.io/Statement/v0.1",
+  "subject": [
+    { "name": "MY-POLICY" }
+  ]
+}
+`
+
+	for _, tt := range []struct {
+		name      string
+		mustErr   bool
+		getSigner func(t *testing.T) bundle.Signer
+	}{
+		{"success", false, func(t *testing.T) bundle.Signer {
+			t.Helper()
+			signer := bundlefakes.FakeSigner{}
+			return &signer
+		}},
+		{"VerifyAttestationContent-fails", true, func(t *testing.T) bundle.Signer {
+			t.Helper()
+			signer := bundlefakes.FakeSigner{}
+			signer.VerifyAttestationContentReturns(errors.New("invalid attesatation"))
+			return &signer
+		}},
+		{"GetKeyPair-fails", true, func(t *testing.T) bundle.Signer {
+			t.Helper()
+			signer := bundlefakes.FakeSigner{}
+			signer.GetKeyPairReturns(nil, errors.New("failed creating keypair"))
+			return &signer
+		}},
+		{"GetAmbientTokens-fails", true, func(t *testing.T) bundle.Signer {
+			t.Helper()
+			signer := bundlefakes.FakeSigner{}
+			signer.GetAmbientTokensReturns(errors.New("fetchin ambient tokens failed"))
+			return &signer
+		}},
+		{"GetOidcToken-fails", true, func(t *testing.T) bundle.Signer {
+			t.Helper()
+			signer := bundlefakes.FakeSigner{}
+			signer.GetOidcTokenReturns(errors.New("getting oidc token fails"))
+			return &signer
+		}},
+		{"BuildSigstoreSignerOptions-fails", true, func(t *testing.T) bundle.Signer {
+			t.Helper()
+			signer := bundlefakes.FakeSigner{}
+			signer.BuildSigstoreSignerOptionsReturns(nil, errors.New("getting signer options fails"))
+			return &signer
+		}},
+		{"SignBundle-fails", true, func(t *testing.T) bundle.Signer {
+			t.Helper()
+			signer := bundlefakes.FakeSigner{}
+			signer.SignBundleReturns(nil, errors.New("signing bundle failed"))
+			return &signer
+		}},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			sut := &Signer{
+				Options:      options.DefaultSigner,
+				bundleSigner: tt.getSigner(t),
+			}
+			res, err := sut.SignStatement([]byte(attData))
+			if tt.mustErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.NotNil(t, res)
+		})
+	}
+}
