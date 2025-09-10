9b178810b0afafc6287ccb0cfa186704bdd16c35
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-09-09 20:35:03 -0600

    Use public key in key parser
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- key/parser.go
+++ key/parser.go
@@ -4,10 +4,10 @@
 package key
 
 import (
-	"crypto/dsa"
-	"crypto/ecdh"
+	"crypto"
 	"crypto/ecdsa"
 	"crypto/ed25519"
+	"crypto/elliptic"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
@@ -23,7 +23,7 @@ func NewParser() *Parser {
 type Parser struct{}
 
 // ParsePublicKey parses a public key that can be used to verify
-func (p *Parser) ParsePublicKey(pubKeyData []byte) (*Public, error) {
+func (p *Parser) ParsePublicKey(scheme Scheme, pubKeyData []byte) (*Public, error) {
 	return parseKeyBytes(pubKeyData)
 }
 
@@ -39,17 +39,38 @@ func parseKeyBytes(pubKeyData []byte) (*Public, error) {
 		return nil, fmt.Errorf("parsing public key from PEM data: %w", err)
 	}
 
-	ret := &Public{
-		Scheme: "",
-		Data:   string(pubKeyData),
-		Key:    pub,
+	k := &Public{
+		Data: string(pubKeyData),
+		Key:  pub,
 	}
 
-	switch pub.(type) {
-	case *rsa.PublicKey, *dsa.PublicKey, *ecdsa.PublicKey,
-		ed25519.PublicKey, *ecdh.PublicKey:
-		return ret, nil
+	switch ik := pub.(type) {
+	case *rsa.PublicKey:
+		k.Type = RSA
+	case *ecdsa.PublicKey:
+		k.Type = ECDSA
+		// We can infer the hash type from the elliptic curve in use. If it does
+		// not match, verifications will not match but they would be insecure anyway
+		switch ik.Curve.Params().Name {
+		case elliptic.P224().Params().Name: // "P-256"
+			k.HashType = crypto.SHA224
+			k.Scheme = EcdsaSha2nistP224
+		case elliptic.P256().Params().Name: // "P-256"
+			k.HashType = crypto.SHA256
+			k.Scheme = EcdsaSha2nistP256
+		case elliptic.P384().Params().Name: // "P-384"
+			k.HashType = crypto.SHA384
+			k.Scheme = EcdsaSha2nistP384
+		case elliptic.P521().Params().Name: // P-521
+			k.HashType = crypto.SHA512
+			k.Scheme = EcdsaSha2nistP521
+		}
+	case ed25519.PublicKey:
+		k.Type = ED25519
+
 	default:
 		return nil, fmt.Errorf("unsupported key type: %T", pub)
 	}
+
+	return k, nil
 }
