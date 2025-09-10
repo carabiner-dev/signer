6e086491604b9d2abaf76e5b2e9775276f61c76f
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-09-09 20:33:35 -0600

    Implement DSSE/PAE encoding
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- dsse/verifier.go
+++ dsse/verifier.go
@@ -4,16 +4,22 @@
 package dsse
 
 import (
+	"crypto"
+	"errors"
 	"fmt"
+	"os"
+	"time"
 
-	"github.com/carabiner-dev/signer/options"
 	sdsse "github.com/sigstore/protobuf-specs/gen/pb-go/dsse"
-	"github.com/sigstore/sigstore-go/pkg/verify"
 	"google.golang.org/protobuf/encoding/protojson"
+
+	"github.com/carabiner-dev/signer/key"
+	"github.com/carabiner-dev/signer/options"
 )
 
 type Verifier interface {
-	RunVerification(*options.Verifier, *sdsse.Envelope) (*verify.VerificationResult, error)
+	RunVerification(*options.Verifier, *key.Verifier, *sdsse.Envelope, []*key.Public) (*key.VerificationResult, error)
+	BuildKeyVerifier(*options.Verifier) (*key.Verifier, error)
 	OpenEnvelope(string) (*sdsse.Envelope, error)
 }
 
@@ -21,17 +27,104 @@ type DefaultVerifier struct{}
 
 // RunVerification verifies the DSSE envelope
 func (dv *DefaultVerifier) RunVerification(
-	opts *options.Verifier, env *sdsse.Envelope,
-) (*verify.VerificationResult, error) {
+	opts *options.Verifier, kv *key.Verifier, env *sdsse.Envelope, keys []*key.Public,
+) (*key.VerificationResult, error) {
+	if env == nil {
+		return nil, fmt.Errorf("")
+	}
 
-	return nil, nil
+	if kv == nil {
+		return nil, errors.New("did not get a key verifier to check signatures")
+	}
+
+	digests := map[crypto.Hash][]byte{}
+	digestStrs := map[string]string{}
+	for _, k := range keys {
+		if _, ok := digests[k.HashType]; ok {
+			continue
+		}
+		// Hash in this hasher
+		digest, err := hashPayload(env, k.HashType)
+		if err != nil {
+			return nil, fmt.Errorf("error hashing with %T: %w", k.HashType, err)
+		}
+		digests[k.HashType] = digest
+		digestStrs[k.HashType.String()] = fmt.Sprintf("%x", digest)
+	}
+
+	// Got all required hashes, verify
+	for _, sig := range env.GetSignatures() {
+		for _, k := range keys {
+			pass, err := kv.VerifyDigest(k, digests[k.HashType], sig.GetSig())
+			if err == nil {
+				if pass {
+					return &key.VerificationResult{
+						Key:      k,
+						Time:     time.Now(),
+						Digest:   digestStrs,
+						Verified: true,
+					}, nil
+				}
+			}
+		}
+	}
+	return &key.VerificationResult{
+		Key:      nil,
+		Time:     time.Now(),
+		Digest:   digestStrs,
+		Verified: false,
+	}, nil
+}
+
+// BuildKeyVerifier builds a key verifier used to check the DSSE signatures
+func (dv *DefaultVerifier) BuildKeyVerifier(opts *options.Verifier) (*key.Verifier, error) {
+	v := key.NewVerifier()
+	return v, nil
 }
 
 // OpenEnvelope parses a DSSE envelope
-func (dv *DefaultVerifier) OpenEnvelope(data string) (*sdsse.Envelope, error) {
+func (dv *DefaultVerifier) OpenEnvelope(path string) (*sdsse.Envelope, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("opening DSSE envelope file: %w", err)
+	}
+
 	env := &sdsse.Envelope{}
-	if err := protojson.Unmarshal([]byte(data), env); err != nil {
+	if err := protojson.Unmarshal(data, env); err != nil {
 		return nil, fmt.Errorf("unmarshaling DSSE envelope: %w", err)
 	}
 	return env, nil
+}
+
+// hashPayload hashes the envelope payload with the supplied hasher
+func hashPayload(env *sdsse.Envelope, hasher crypto.Hash) ([]byte, error) {
+	if env == nil {
+		return nil, errors.New("no envelope supplied")
+	}
+
+	if env.Payload == nil {
+		return nil, errors.New("DSEE envelope has no payload")
+	}
+
+	if env.GetPayloadType() == "" {
+		return nil, errors.New("unset payload type in DSSE envelope")
+	}
+
+	paeEncoded := paeEncode(env)
+
+	h := hasher.New()
+	if _, err := h.Write(paeEncoded); err != nil {
+		return nil, fmt.Errorf("writing to hasher: %w", err)
+	}
+	return h.Sum(nil), nil
+}
+
+// paeEncode implements the DSSE signing protocol:
+// https://github.com/secure-systems-lab/dsse/blob/master/protocol.md#signature-definition
+// This function was stolen from the Secure System Labs dsse packager.
+func paeEncode(env *sdsse.Envelope) []byte {
+	// payloadType string, payload []byte) []byte {
+	return []byte(fmt.Sprintf("DSSEv1 %d %s %d %s",
+		len(env.GetPayloadType()), env.GetPayloadType(),
+		len(env.GetPayload()), env.GetPayload()))
 }
